1e6467009c02d70e796dc091615da084a9379046
Gui-Correa <71907354+Gui-Correa@users.noreply.github.com>
2020-10-02 22:23:29 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercicio_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.WriteLine("Digite o valor de A: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor de B: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            c = a;
+            a = b;
+            b = c;
+
+            Console.WriteLine("Valor de a= " + a);
+            Console.WriteLine("Valor de b= " + b);
+
+            Console.ReadKey();
+
+        }
+    }
+}
